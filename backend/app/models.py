931d385401032bbd2f1b6e8e921bdfc8325217a7
Binil K Joseph <binilkjoseph@gmail.com>
2026-01-11 23:06:16 +0530

    Updated Array with JSON

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,6 +1,11 @@
 from sqlalchemy import Column, Integer, String, Float
 from sqlalchemy.dialects.postgresql import ARRAY
 from .database import Base
+from sqlalchemy import Column, Integer, String, Float
+from sqlalchemy.types import JSON
+
+
+
 
 class Product(Base):
     __tablename__ = "products"
@@ -9,7 +14,7 @@ class Product(Base):
     name = Column(String, nullable=False)
     unit_price = Column(Float, nullable=False)
     current_stock = Column(Integer, nullable=False)
-    monthly_sales = Column(ARRAY(Integer), nullable=False)
+    monthly_sales = Column(JSON, nullable=False)
 
 class User(Base):
     __tablename__ = "users"
