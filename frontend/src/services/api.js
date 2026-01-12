a535d7194049e70b3de86c092da9c2b120419d0c
Binil K Joseph <binilkjoseph@gmail.com>
2026-01-12 19:18:00 +0530

    frontend url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://smart-inventory-forecaster.onrender.com/",
+  baseURL: "https://smart-inventory-forecaster.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
